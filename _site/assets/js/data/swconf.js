f100d2841e65cb3de68a58160004c9565da989f3
Daniel <danielmesquitadev@gmail.com>
2024-12-22 05:50:33 -0300

    fix: update jekyll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734855503',resources: [
+    cacheName: 'chirpy-1734857338',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
