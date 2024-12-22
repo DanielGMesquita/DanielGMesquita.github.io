60480310497554c38e8d8794d973676fbe364471
Daniel <danielmesquitadev@gmail.com>
2024-12-22 07:54:38 -0300

    fix: install bootstrap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734864652',resources: [
+    cacheName: 'chirpy-1734864870',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
