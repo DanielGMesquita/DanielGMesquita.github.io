09fb38a274a74c4f3586d84e1de59a0e7062cefc
Daniel <danielmesquitadev@gmail.com>
2026-02-15 13:44:04 -0300

    :fix: empty href errors fixes

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771173298',resources: [
+    cacheName: 'chirpy-1771173808',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
